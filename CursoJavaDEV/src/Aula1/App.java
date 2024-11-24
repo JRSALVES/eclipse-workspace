32dcb47f20cdcd0e27d9a90203be1e92ae71a408
JRSALVES <jeruso.nascimento@outlook.com>
2024-11-24 15:15:59 -0300

    Iniciando novo projeto

---
+++ CursoJavaDEV/src/Aula1/App.java
@@ -0,0 +1,15 @@
+package Aula1;
+
+public class App {
+    public static void main(String[] args) {
+
+        Pessoa aluno1 = new Pessoa();
+
+        aluno1.setNome("Jeruso");
+        aluno1.setSobrenome("Alves Nascimento");
+        aluno1.setIdade(57);
+
+        // Corrigido para melhorar a legibilidade da saída
+        System.out.println("Nome: " + aluno1.getNome() + " \nSobrenome: " + aluno1.getSobrenome() + " \nIdade: " + aluno1.getIdade());
+    }
+}
